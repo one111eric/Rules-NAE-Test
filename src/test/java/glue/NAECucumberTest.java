9a8c67de4274248bbd17a0639b34297fb32358f3
mxiang001c <Miao_Xiang@cable.comcast.com>
2015-10-01 13:10:19 -0700

    include original E2E tests again.

--- src/test/java/glue/NAECucumberTest.java
+++ src/test/java/glue/NAECucumberTest.java
@@ -22,8 +22,8 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(plugin={"pretty","html:target/cucumber","json:target/cucumber.json"},glue={"glue"},features={"src/test/resources/test_features/" },
 
-  tags={"@EndToEndMT-XRULES-1168"})
-//@EndToEnd-XRULES-347,
+  tags={"@EndToEnd-XRULES-347,@EndToEndMT-XRULES-1168"})
+
 
 @Test
 public class NAECucumberTest extends AbstractTestNGCucumberTests{
