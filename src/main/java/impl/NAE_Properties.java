cc6a195fb5773df7c46f6a67cd32bf969312044e
mxiang001c <Miao_Xiang@cable.comcast.com>
2015-10-21 17:19:03 -0700

    e2e for tps initial change

--- src/main/java/impl/NAE_Properties.java
+++ src/main/java/impl/NAE_Properties.java
@@ -5,19 +5,19 @@ package impl;
  * Mainly server endpoints
  */
 public class NAE_Properties {
-	public static final String NAE_URL = "http://nae.qa.i.xrs.vacsv.com/notify";
+	public static final String NAE_URL = "http://nae.tps.i.xrs.vacsv.com/notify";
 	public static final String MOCK_SERVER = "mock.rules.vacsv.com";
 	public static final int MOCK_SERVER_PORT = 8080;
 	public static final boolean WITH_X_DEBUG_HEADER = true;
 	public static final boolean WITHOUT_X_DEBUG_HEADER = false;
 	public static final String MOCKSERVER_ADDRESS = "http://mock.rules.vacsv.com:8080";
-	public static final String EEL_ENDPOINT = "http://eel.qa.rules.comcast.com/";
-	public static final String REST_ENDPOINT = "http://rest.qa.rules.comcast.com/";
-	public static final String PROVISION_ENDPOINT = "http://rest.qa.rules.comcast.com/provisions";
-	public static final String EEL_EVENT_ENDPOINT = "http://eel.qa.rules.comcast.com/elementsevent";
-    public static final String EEL_HEALTH_ENDPOINT= "http://eel.qa.rules.comcast.com/health";
+	public static final String EEL_ENDPOINT = "http://eel.tps.rules.comcast.com/";
+	public static final String REST_ENDPOINT = "http://rest.tps.rules.comcast.com/";
+	public static final String PROVISION_ENDPOINT = "http://rest.tps.rules.comcast.com/provisions";
+	public static final String EEL_EVENT_ENDPOINT = "http://eel.tps.rules.comcast.com/elementsevent";
+    public static final String EEL_HEALTH_ENDPOINT= "http://eel.tps.rules.comcast.com/health";
     public static final String LOCATION_ENDPOINT = REST_ENDPOINT + "locations/";
-	public static final String MOLECULE_ENDPOINT = "http://molecule.qa.i.xrs.vacsv.com/";
+	public static final String MOLECULE_ENDPOINT = "http://molecule.tps.i.xrs.vacsv.com/";
 	public static final String MOLECULE_MAPPING_ENDPOINT = MOLECULE_ENDPOINT
 			+ "mappings/xh/";
 }
--- src/test/java/glue/NAECucumberTest.java
+++ src/test/java/glue/NAECucumberTest.java
@@ -39,8 +39,8 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(plugin={"pretty","html:target/cucumber","json:target/cucumber.json"},glue={"glue"},features={"src/test/resources/test_features/" },
 
-  tags={"@EndToEndMT-XRULES-1168, @EndToEndTTL-XRULES-1530, @EndToEnd-XRULES-347"})
-// 
+  tags={"@EndToEndTTL-XRULES-1530, @EndToEnd-XRULES-347"})
+//@EndToEndMT-XRULES-1168, 
 
 @Test
 public class NAECucumberTest extends AbstractTestNGCucumberTests{
