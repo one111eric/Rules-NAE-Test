05dadbdf072b2a265f8370a312934b5a5b46d5fb
mxiang001c <Miao_Xiang@cable.comcast.com>
2015-10-29 18:26:02 -0700

    adding after test function to delete locations/sites

--- src/test/java/glue/NAECucumberTest.java
+++ src/test/java/glue/NAECucumberTest.java
@@ -1,15 +1,26 @@
 package glue;
 
+import impl.SiteAndLocation;
+
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
 import org.apache.log4j.Logger;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+
+
+
+
 
 
 
@@ -55,6 +66,36 @@ public class NAECucumberTest extends AbstractTestNGCucumberTests{
 		catch(IOException e){
 			LOGGER.debug(e);
 		}
+	}
+	
+	@AfterTest
+	public void clearSitesAndLocations(){
+		SiteAndLocation newSiteAndLocation=new SiteAndLocation();
+		String sitesFile="src/test/resources/test_data/sites.txt";
+		String locationsFile="src/test/resources/test_data/locations.txt";
+		
+		try(BufferedReader br=new BufferedReader(new FileReader(sitesFile))){
+			String line;
+			while((line=br.readLine())!=null){
+				newSiteAndLocation.deleteSite(line);
+			}
+		} catch (FileNotFoundException e) {
+			LOGGER.error(e);		} 
+		  catch (IOException e) {
+			LOGGER.error(e);
+		}
+		
+		try(BufferedReader br=new BufferedReader(new FileReader(locationsFile))){
+			String line;
+			while((line=br.readLine())!=null){
+				newSiteAndLocation.deleteLocation(line);
+			}
+		} catch (FileNotFoundException e) {
+			LOGGER.error(e);		} 
+		  catch (IOException e) {
+			LOGGER.error(e);
+		}
+		
 	}
 }
 
